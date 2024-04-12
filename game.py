e0d7bc8bd11a7dd72d5b1ec3b4b87b00298e5940
marisgan <marisgan@gmail.com>
2024-04-12 18:03:12 +0300

    Final working version

--- game.py
+++ game.py
@@ -11,23 +11,29 @@ PERSONS_NAMES = [
 ]
 persons_names = PERSONS_NAMES.copy()
 
+THINGS_NUM = 50
 MAX_THINGS_FOR_PERSON = 4
 PERSONS_COUNT = 10
-HEALTH_POINTS = 100
+BASE_HP = 50
 BASE_ATTACK = 10
 BASE_PROTECTION = 0.01
 
 
 def set_name(names):
+    """Выбираем уникальное рандомное имя из списка имён"""
     return names.pop(randint(0, len(names) - 1))
 
 
-class Thing(): 
-    def __init__(self): 
+class Thing():
+    """Класс игровых вещей"""
+    def __init__(self):
         self.name = choice(THINGS_NAMES)
         self.defence = randint(0, MAX_DEFENCE) / 100
         self.damage = randint(0, MAX_DAMAGE_HP)
         self.hp = randint(0, MAX_DAMAGE_HP)
+
+    def __str__(self):
+        return f'{self.name} {self.defence} {self.damage} {self.hp}'
 
 
 class Person:
@@ -35,86 +41,93 @@ class Person:
 
     def __init__(
             self,
-            health_points=HEALTH_POINTS,
+            base_hp=BASE_HP,
             base_attack=BASE_ATTACK,
             base_protection=BASE_PROTECTION
     ):
         self.name = set_name(persons_names)
-        self.base_health_points = health_points
-        self.base_attack = base_attack
-        self.base_protection = base_protection
+        self.hp = base_hp
+        self.attack = base_attack
+        self.protection = base_protection
 
     def __str__(self):
         return self.name
 
-   def set_things(self, things):
-        """Одеваем персонажа рандомными вещами и устанавливаем финальные характеристики"""
+    def set_things(self, things):
+        """Одеваем вещи на персонажа, применяем их характеристики"""
         self.persons_things = []
         for _ in range(randint(1, MAX_THINGS_FOR_PERSON)):
-            self.persons_things.append(things.pop())
+            self.persons_things.append(things.pop(randint(0, len(things) - 1)))
         for thing in self.persons_things:
-            self.final_health_points = self.base_health_points + thing.hp
-            self.final_attack = self.base_attack + thing.damage
-            self.final_protection = self.base_protection + thing.defence
+            self.hp += thing.hp
+            self.attack += thing.damage
+            self.protection += thing.defence
 
-    def attack_damage(self, attacking):
-        self.final_health_points = self.final_health_points - attacking.final_attack/self.final_protection
+    def decrease_hp(self, attack_damage):
+        """Метод для уменьшения жизней во время боя"""
+        self.hp -= attack_damage
+
 
 class Paladin(Person):
 
     def __init__(self):
         super().__init__()
-        self.base_health_points *= 2
-        self.base_protection *= 2
+        self.hp *= 2
+        self.protection *= 2
 
 
 class Warrior(Person):
 
     def __init__(self):
         super().__init__()
-        self.base_attack *= 2
+        self.attack *= 2
 
 
 def main():
-    # Шаг 1. Создаём список вещей
+    """Шаг 1. Создаём список вещей"""
     things = []
-    for i in range(50):
+    for i in range(THINGS_NUM):
         things.append(Thing())
     things.sort(key=lambda x: x.defence)
-    print(things)
 
-    # Шаг 2.
-    paladins = [Paladin() for _ in range(randint(0, PERSONS_COUNT))]
-    warriors = [Warrior() for _ in range(10 - len(paladins))]
+    """Шаг 2. Создаём персонажей"""
+    paladins = [Paladin() for _ in range(randint(0, PERSONS_COUNT - 1))]
+    warriors = [Warrior() for _ in range(PERSONS_COUNT - len(paladins))]
     persons = paladins + warriors
 
-    # Шаг 3. Одеваем персонажей
+    """Шаг 3. Одеваем персонажей"""
     for person in persons:
         person.set_things(things)
+        print(
+            f'Участник: {person}, {type(person).__name__}, '
+            f'Здоровье: {person.hp}, Атака: {person.attack}, '
+            f'Защита: {round(person.protection, 5)}'
+        )
 
-    # Шаг 4. Отправляем персонажей на арену
+    """Шаг 4. Отправляем персонажей на арену"""
     while len(persons) > 1:
-        attacking = choice(persons)
-        persons.pop(attacking)
-        defending = choice(persons)
-        persons.pop(defending)
-        attacking_hp = attacking.final_health_points
-        defending_hp = defending.final_health_points
-        while attacking.final_health_points > 0 or defending.final_health_points> 0:
-            defending.attack_damage(attacking)
-            print(f'{attacking.name} наносит удар по {defending.name} на {defending.final_health_points} урона')
-            new_attacing = defending
-            defending = attacking
-            attacking = new_attacing
-        if attacking.final_health_points > 0:
-            attacking.final_health_points = attacking_hp
-            print(f'{attacking.name} победил {defending.name}')
-            persons.append(attacking)
-        else:
-            defending.final_health_points = defending_hp
-            print(f'{defending.name} победил {attacking.name}')
-            persons.append(defending)
-        
+        attacker = persons.pop(randint(0, len(persons) - 1))
+        defender = persons.pop(randint(0, len(persons) - 1))
+        while True:
+            attack_damage = (
+                attacker.attack - attacker.attack * defender.protection
+            )
+            defender.decrease_hp(attack_damage)
+            print(
+                f'{attacker.name} наносит удар по {defender.name} '
+                f'на {round(attack_damage, 5)} урона'
+            )
+            if defender.hp < 0:
+                persons.append(attacker)
+                print(
+                    f'В поединке побеждает {attacker.name}. '
+                    f'{defender.name} выбывает.'
+                )
+                break
+            else:
+                attacker, defender = defender, attacker
+    print(f'Финальный победитель: {persons.pop().name}')
+
 
 if __name__ == '__main__':
     main()
