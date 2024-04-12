2551f88756a02cca66b8b35e66d487b04e88acf4
Evgeniy Akimov <akimoveug@gmail.com>
2024-04-12 13:07:14 +0300

    add game logic steps 1, 3, 4

--- game.py
+++ game.py
@@ -8,19 +8,37 @@ MAX_DAMAGE_HP = 20
 class Thing(): 
     def __init__(self): 
         self.name = random.choice(NAMES)
-        self.defence = random.randint(0, MAX_DEFENCE)
+        self.defence = random.randint(0, MAX_DEFENCE) / 100
         self.damage = random.randint(0, MAX_DAMAGE_HP)
         self.hp = random.randint(0, MAX_DAMAGE_HP)
 
 
 def main():
-
+    # Шаг 1. Создаём список вещей
     things = []
     for i in range(10):
         things.append(Thing())
     things.sort(key=lambda x: x.defence)
     print(things)
 
+    persons = []
+    # Шаг 3. Одеваем персонажей
+    for person in persons:
+        person.set_things(things)
+
+    # Шаг 4. Отправляем персонажей на арену
+    while len(persons) > 1
+        attacking = random.choice(persons)
+        persons.pop(attacking)
+        defending = random.choice(persons)
+        persons.pop(defending)
+        
+        while attacking.hp > 0 or defending.hp > 0:
+            
+            print(f'{attacking.name} наносит удар по {defending.name} на {кол-во урона} урона')
+            new_attacing = defending
+            defending = attacking
+            attacking  = new_attacing
 
 if __name__ == '__main__':
     main()
